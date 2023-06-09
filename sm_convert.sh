e2635855afca16c48130335c00feb0504d45ec48
lcrosvila <lcrosvila@gmail.com>
2023-06-09 11:40:54 +0200

    deleted the new way of parsing sm files, did not work

--- sm_convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in /Users/lcros/Documents/ddc/DDR\ Classics/**/*.sm; do python3 sm_to_bin.py "$f"; done
-
-for f in /Users/lcros/Documents/ddc/DDR\ Classics/**/*bin.npy; do mv "$f" "$PWD/converted_charts/ai_sm_bin"; done
-
-for f in /Users/lcros/Documents/ddc/DDR\ Classics/**/*.npy; do mv "$f" "$PWD/converted_charts/ai_sm_time"; done
